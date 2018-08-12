f2f634c647ebe2a5dfc7a239de315672e4a6bb78
mikhail plavskiy <mikhail.plavskiy@quest.com>
2018-08-12 19:50:39 +0000

    added log analytics

--- example.tf
+++ example.tf
@@ -12,3 +12,10 @@ resource "azurerm_eventgrid_topic" "mp_eventgrid" {
     location = "${azurerm_resource_group.mp_terraform.location}"
     resource_group_name = "${azurerm_resource_group.mp_terraform.name}"
 }
+
+resource "azurerm_log_analytics_workspace" "mp_la" {
+    name = "mp-la"
+    location = "eastus"
+    resource_group_name = "${azurerm_resource_group.mp_terraform.name}"
+    sku = "Standard"
+}
