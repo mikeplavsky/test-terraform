d2f32cff289bbf4425953acb9230c3c7fc4d447b
mikhail plavskiy <mikhail.plavskiy@quest.com>
2018-08-12 19:39:42 +0000

    added eventgrid

--- example.tf
+++ example.tf
@@ -3,5 +3,12 @@ provider "azurerm" {}
 resource "azurerm_resource_group" "mp_terraform" {
     name = "terraform"
     location = "West US"
-    tags = {"product" = "rmaz", "platform" = "mmp"}
+    tags = {
+        "product" = "rmaz", 
+        "platform" = "mmp"}}
+
+resource "azurerm_eventgrid_topic" "mp_eventgrid" {
+    name = "mp-eventgrid"
+    location = "${azurerm_resource_group.mp_terraform.location}"
+    resource_group_name = "${azurerm_resource_group.mp_terraform.name}"
 }
